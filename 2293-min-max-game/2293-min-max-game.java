110c8963eba1c299bab86fbb920a9caa70bdc99f
yuvaraj_r <yuvaraj13102006@gmail.com>
2026-06-11 09:44:04 +0530

    Time: 0 ms (100%), Space: 45.2 MB (35.07%) - LeetHub

--- 2293-min-max-game/2293-min-max-game.java
+++ 2293-min-max-game/2293-min-max-game.java
@@ -1,27 +1,16 @@
 class Solution {
     public int minMaxGame(int[] nums) {
         int n = nums.length;
-        
-        // Continue the process until only 1 element remains
-        while (n > 1) {
-            // Create the new array with half the length
-            int[] newNums = new int[n / 2];
-            
-            // Populate the new array based on the alternating min/max rules
-            for (int i = 0; i < n / 2; i++) {
-                if (i % 2 == 0) {
-                    newNums[i] = Math.min(nums[2 * i], nums[2 * i + 1]);
-                } else {
-                    newNums[i] = Math.max(nums[2 * i], nums[2 * i + 1]);
-                }
-            }
-            
-            // Replace the old array with the new array and update n
-            nums = newNums;
-            n = nums.length;
+        if(n==1) {
+            return nums[0];
         }
-        
-        // The last remaining number is at index 0
-        return nums[0];
+        int[] na = new int[n/2];
+        for(int i=0;i<n/2;i+=2) {
+            na[i]=Math.min(nums[2*i],nums[2*i+1]);
+        }
+        for(int i=1;i<n/2;i+=2) {
+            na[i]=Math.max(nums[2*i],nums[2*i+1]);
+        }
+        return minMaxGame(na);
     }
 }
