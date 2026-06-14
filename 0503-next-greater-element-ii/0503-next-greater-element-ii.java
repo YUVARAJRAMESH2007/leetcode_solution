9a9430dfc901381de8f2e866ab6428171e96805b
yuvaraj_r <yuvaraj13102006@gmail.com>
2026-06-14 21:25:59 +0530

    Time: 15 ms (57.77%), Space: 48.5 MB (45.82%) - LeetHub

--- 0503-next-greater-element-ii/0503-next-greater-element-ii.java
+++ 0503-next-greater-element-ii/0503-next-greater-element-ii.java
@@ -3,34 +3,27 @@ import java.util.Stack;
 
 class Solution {
     public int[] nextGreaterElements(int[] nums) {
-        int n = nums.length;
-        int[] result = new int[n];
-        Arrays.fill(result, -1);
-        
-        Stack<Integer> stack = new Stack<>();
-        
-        // TRICK 1: Loop runs for 2 * n times! (Rendu round varom)
-        for (int i = 0; i < 2 * n; i++) {
-            
-            // TRICK 2: Modulo magic to wrap around
-            // Example: i = 3, n = 3 -> 3 % 3 = 0 (Starts from beginning again)
-            int currentIndex = i % n; 
-            int currentNumber = nums[currentIndex];
-            
-            // The exact same BOUNCER rule you wrote!
-            while (!stack.isEmpty() && currentNumber > nums[stack.peek()]) {
-                int poppedIndex = stack.pop();
-                result[poppedIndex] = currentNumber;
+        int n=nums.length;
+        int result[]=new int[n];
+        Stack<Integer> stack=new Stack<>();
+        for(int i=2*n-1;i>=0;i--){
+            int currentIndex=i%n;
+            int currentnum=nums[currentIndex];
+            while(!stack.isEmpty() && stack.peek()<=currentnum){
+                stack.pop();
             }
-            
-            // TRICK 3: First round la mattum thaan aalungala waiting room la (stack la) vidanum.
-            // Second round varumbothu, erkanave wait panravangala thuratha mattum thaan round varom, 
-            // puthusa yarayum ulla anupakoodathu!
-            if (i < n) {
-                stack.push(currentIndex);
+            if(i<n){
+                if(stack.isEmpty()){
+                    result[currentIndex]=-1;
+                }
+                else{
+                    result[currentIndex]=stack.peek();
+                }
+
             }
+            stack.push(currentnum);
+
         }
-        
         return result;
     }
 }
